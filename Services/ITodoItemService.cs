d32d1d33165246b865e100004530104d88163586
Tenori Taiga <adam.wawrzenczyk@gmail.com>
2018-03-26 11:19:24 +0200

    Added db connection

--- Services/ITodoItemService.cs
+++ Services/ITodoItemService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AspNetCoreTodo.Models;
+using Humanizer;
 
 namespace AspNetCoreTodo.Services
 {
---
+++ Services/TodoItemService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetCoreTodo.Models;
+using Microsoft.EntityFrameworkCore;
+using AspNetCoreTodo.Data;
+
+namespace AspNetCoreTodo.Services
+{
+    public class TodoItemService: ITodoItemService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TodoItemService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<TodoItem>> GetIncompleteItemsAsync()
+        {
+            var items = await _context.Items
+                .Where(x => !x.IsDone)
+                .ToArrayAsync();
+
+            return items;
+        }
+    }
+}
